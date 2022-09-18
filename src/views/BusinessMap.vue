bcdaf7f6ca56da3cd6dde2346c175182d18ec6dc
zltyrzshy <q3251778551@gmail.com>
2022-09-18 21:52:24 +0800

    new_commit

--- src/views/BusinessMap.vue
+++ src/views/BusinessMap.vue
@@ -1,7 +1,21 @@
 <template>
 	<div class="map">
-		<baidu-map class="mapId" :center="center" :zoom="zoom" :scroll-wheel-zoom="false" @moving="syncCenterAndZoom"
+		<baidu-map class="mapId" :center="center" :zoom="zoom" :scroll-wheel-zoom="true" @moving="syncCenterAndZoom"
 			@moveend="syncCenterAndZoom" @zoomend="syncCenterAndZoom" @ready="readyMap">
+
+			<li v-for="item in businessArr" @click="infoWindowOpen(item.businessId)">
+				<bm-marker :position="{lng: item.lng, lat: item.lat}" :dragging="true"
+					animation="BMAP_ANIMATION_BOUNCE">
+					<bm-label content=item.businessName :labelStyle="{color: 'black', fontSize : '24px'}" />
+				</bm-marker>
+			</li>
+			<!-- 缩放比例尺 -->
+			<bm-navigation anchor="BMAP_ANCHOR_TOP_RIGHT"></bm-navigation>
+
+			<bm-geolocation anchor="BMAP_ANCHOR_BOTTOM_RIGHT" :showAddressBar="true" :autoLocation="true">
+			</bm-geolocation>
+
+			<bm-marker :position="{lng:117.31851, lat: 39.003545}"></bm-marker>
 		</baidu-map>
 	</div>
 </template>
@@ -11,17 +25,26 @@
 		data() {
 			return {
 				center: {
-					lng: 117.339417,
+					lng: 117.31851,
 					lat: 39.003545
 				}, //初始的中心位置
-				zoom: 13, //缩放级别
+				zoom: 15, //缩放级别
 				// points: [], //打点的地方
 				//百度地图初始化数据
 				baidumapSwitch: false,
+				businessArr: [],
+				user: {}
 			}
 		},
-		mounted() {
+		created() {
+			this.user = this.$getSessionStorage('user');
+			//获取所有商家信息
+			this.$axios.post('BusinessController/listBusinessByOrderTypeId').then(response => {
+				this.businessArr = response.data;
 
+			}).catch(error => {
+				console.error(error);
+			});
 		},
 		methods: {
 			//地图实例:初始化加载地图
@@ -29,15 +52,8 @@
 				BMap,
 				map
 			}) {
-				// console.log(BMap, "BMap");
-				// console.log(map, "map");
+				map.enableScrollWheelZoom();
 			},
-			// clickHandler(e) {
-			// 	console.log(e.point.lng);
-			// 	console.log(e.point.lat);
-			// 	this.center.lng = e.point.lng;
-			// 	this.center.lat = e.point.lat;
-			// },
 			syncCenterAndZoom(e) {
 				const {
 					lng,
@@ -47,20 +63,11 @@
 				this.center.lat = lat
 				this.zoom = e.target.getZoom()
 				console.log(this.zoom);
+			},
+			infoWindowOpen(businessId){
+				
 			}
 		}
-		//添加海量点
-		// addPoints() {
-		// 	const points = [];
-		// 	for (var i = 0; i < n; i++) {
-		// 		const position = {
-		// 			lng: Math.random() * 40 + 85,
-		// 			lat: Math.random() * 30 + 21
-		// 		}
-		// 		points.push(position)
-		// 	}
-		// 	this.points = points
-		// }
 
 	}
 </script>
