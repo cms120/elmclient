d15a94b241828cbac3db927334815d60c4f7543e
Shan <zwh.20@qq.com>
2022-10-11 15:36:30 +0800

    update format

--- src/main.js
+++ src/main.js
@@ -6,13 +6,14 @@ import axios from 'axios'
 import qs from 'qs'
 import {
 	getCurDate,
-	setSessionStorage,
+	getLocalStorage,
 	getSessionStorage,
+	removeLocalStorage,
 	removeSessionStorage,
 	setLocalStorage,
-	getLocalStorage,
-	removeLocalStorage
+	setSessionStorage
 } from './common.js'
+
 Vue.config.productionTip = false
 //设置axios的基础url部分
 axios.defaults.baseURL = 'http://localhost:8080/elm/';
@@ -26,20 +27,20 @@ Vue.prototype.$removeSessionStorage = removeSessionStorage;
 Vue.prototype.$setLocalStorage = setLocalStorage;
 Vue.prototype.$getLocalStorage = getLocalStorage;
 Vue.prototype.$removeLocalStorage = removeLocalStorage;
-router.beforeEach(function(to, from, next) {
-			let user = sessionStorage.getItem('user');
-			// 除了登录、注册、首页、商家列表、商家信息之外，都需要判断是否登录
-			if (!
-				(to.path === '/' || to.path === '/index' || to.path === '/businessList' || to.path === '/businessInfo' || to.path === '/login'||to.path==='/register')){
-					if (user == null) {
-						router.push('/login');
-						location.reload();
-					}
-				}
-				next();
-			});
+router.beforeEach(function (to, from, next) {
+    let user = sessionStorage.getItem('user');
+    // 除了登录、注册、首页、商家列表、商家信息之外，都需要判断是否登录
+    if (!
+        (to.path === '/' || to.path === '/index' || to.path === '/businessList' || to.path === '/businessInfo' || to.path === '/login' || to.path === '/register')) {
+        if (user == null) {
+            router.push('/login');
+            location.reload();
+        }
+    }
+    next();
+});
 
-		new Vue({
-			router,
-			render: h => h(App)
-		}).$mount('#app')
+new Vue({
+    router,
+    render: h => h(App)
+}).$mount('#app')
